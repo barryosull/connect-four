1757ddf18ab6d9990a70b0e5503775c67bbff995
Barry O'Sullivan <bosulli85@gmail.com>
2025-08-18 00:48:25 +0100

    Test player agent

--- src/controllers/cli/player_agent.py
+++ src/controllers/cli/player_agent.py
@@ -13,13 +13,15 @@ type Actions = list[Action]
 # Player that is controlled by a basic agent
 class PlayerAgent(PlayerInterface):
 
-    def __init__(self, checker: Checker, agent: Agent, renderer: Renderer):
+    def __init__(self, checker: Checker, agent: Agent, renderer: Renderer, sleep_time: int = 2):
         self.checker = checker 
         self.__agent = agent
         self.__renderer = renderer
+        self.__sleep_time = sleep_time
 
     def select_action(self, board: Board) -> Action:
         # Pretend to think
         self.__renderer.print_agent_thinking(self.checker)
-        time.sleep(2)
+        time.sleep(self.__sleep_time)
+
         return self.__agent.select_next_slot(self.checker, board)
